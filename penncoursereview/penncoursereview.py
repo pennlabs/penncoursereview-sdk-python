8770c8584d40ebdaf6c44e29bce8259d09264af5
Adel Qalieh <adelq@users.noreply.github.com>
2015-05-12 03:31:10 -0400

    Simplify token fetching code

--- penncoursereview/penncoursereview.py
+++ penncoursereview/penncoursereview.py
@@ -13,12 +13,8 @@ DOMAIN = "http://api.penncoursereview.com/v1/"
 
 def fetch_pcr(*args, **kwargs):
     """Wrapper for fetch to automatically parse results from the PCR API."""
-    try:
-        # Attempt to load the user's token
-        kwargs['token'] = os.environ["PCR_AUTH_TOKEN"]
-    except KeyError:
-        # On error, use the public token
-        kwargs['token'] = "public"
+    # Load user's token from `PCR_AUTH_TOKEN`, use public token as default if missing
+    kwargs['token'] = os.getenv("PCR_AUTH_TOKEN", "public")
     return fetch(DOMAIN, *args, **kwargs)['result']
 
 
