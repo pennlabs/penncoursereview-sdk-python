ec26a9884fa61aa4eb8c31d263b73671a137c9a4
Adel Qalieh <aqalieh95@gmail.com>
2016-03-09 18:42:38 -0500

    Add automatic deploy process

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-__version_info__ = ('0', '1', '0')
+__version_info__ = ('0', '1', '1')
 __version__ = '.'.join(__version_info__)
 
 # To install the penncoursereview library, open a Terminal shell, then run this
